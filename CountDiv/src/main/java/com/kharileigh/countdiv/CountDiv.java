b2046a1cf59bbf377ecda246b6474e6b2ec01b18
kharileigh <kharileighmiller@gmail.com>
2023-02-18 14:59:21 +0000

    PREFIX SUMS - Count Div completed successfully

---
+++ CountDiv/src/main/java/com/kharileigh/countdiv/CountDiv.java
@@ -0,0 +1,54 @@
+/**
+ *
+ * @author kharileigh
+ * < Write a function:
+
+class Solution { public int solution(int A, int B, int K); }
+
+that, given three integers A, B and K, returns the number of integers within the range [A..B] that are divisible by K, i.e.:
+
+{ i : A ≤ i ≤ B, i mod K = 0 }
+
+For example, for A = 6, B = 11 and K = 2, your function should return 3, because there are three numbers divisible by 2 within the range [6..11], namely 6, 8 and 10.
+
+Write an efficient algorithm for the following assumptions:
+
+A and B are integers within the range [0..2,000,000,000];
+K is an integer within the range [1..2,000,000,000];
+A ≤ B.
+ */
+
+package com.kharileigh.countdiv;
+
+
+public class CountDiv {
+
+    public static void main(String[] args) {
+        
+        CountDiv countDiv = new CountDiv();
+        
+        // should return 3
+        System.out.println(countDiv.solution(6, 11, 2));
+    }
+    
+    
+    public int solution(int A, int B, int K) {
+    
+        // base is the ceiling of A
+        int base = (int)Math.ceil((double) A / K);
+        
+        // multiply base by K to get first number divisible by B
+        base *= K;
+        
+        // to remove B, substract base from it
+        B -= base;
+        
+        // count is the floor of B
+        int count = (int)Math.floor((double) B / K);
+        
+        // add first value onto it
+        count++;
+        
+        return count;
+    }
+}
